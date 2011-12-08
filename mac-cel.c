39979175844137cd424f20bc49d56a2b44db8d83
Chris Down <cdown.uk@gmail.com>
2011-12-08 10:25:18 +0000

    Allow version query with -v

--- mac-cel.c
+++ mac-cel.c
@@ -4,18 +4,26 @@
 #include <IOKit/hidsystem/IOHIDParameter.h>
 #include <IOKit/hidsystem/event_status_driver.h>
 
+#ifndef __APPLE__
+#error This program is only designed to be run/built for Mac OSX.
+#endif
+
 #define VERSION 1.01
 
 int main(int argc, char *argv[]) {
     io_connect_t h;
     const int32_t a = INT32_MIN;
     CFStringRef t;
+    if (argc > 1 && (!(strncmp(argv[1], "-v", 3)))) {
+        puts("mac-cel "VERSION);
+        return 0;
+    }
+
     if ((h = NXOpenEventStatus())) {
-        if (argc > 1 && !(strncmp(argv[1], "-t", 3)))
+        if (argc > 1 && (!(strncmp(argv[1], "-t", 3))))
             t = CFSTR(kIOHIDTrackpadAccelerationType);
         else
             t = CFSTR(kIOHIDMouseAccelerationType);
-
         if (IOHIDSetParameter(h, t, &a, sizeof(a)) != KERN_SUCCESS) {
             fputs("Failed to set HID parameters.\n", stderr);
             NXCloseEventStatus(h);
